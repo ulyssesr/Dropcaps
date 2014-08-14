7f76e9f15607d401557ab647e0ffb037395ce66c
Ulysses <ulyssesr@yahoo.com>
2014-08-14 14:41:53 -0700

    fixed formatting.

--- dropcaps/dropcaps.php
+++ dropcaps/dropcaps.php
@@ -23,12 +23,12 @@ add_action( 'wp_enqueue_scripts', 'urr_dropcaps_css_header' );
 // modify content. find first word. css does first-letter does all the trick.
 
 function urr_dropcaps($content='') {
-    $pos = stripos($content, '<p>');
-    if (($pos !== 0) || ($pos === false)) {
-        return '<p class="dropcaps">' . $content . '</p>';
-    } else {
-        return '<p class="dropcaps"' . stristr($content, '>');
-    }
+	$pos = stripos($content, '<p>');
+	if (($pos !== 0) || ($pos === false)) {
+		return '<p class="dropcaps">' . $content . '</p>';
+	} else {
+		return '<p class="dropcaps"' . stristr($content, '>');
+	}
 }
 add_filter('the_content', 'urr_dropcaps', 7);
 
